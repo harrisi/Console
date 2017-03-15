fcf4e5e22f1fd54a657abe3becbfa3d82a20cb43
Sid Spry <R030t1@gmail.com>
2017-03-15 13:45:52 -0500

    Use glfwWaitEvents() instead of glfwPollEvents() for asynchronous drawing.

--- Console/Console.cpp
+++ Console/Console.cpp
@@ -4,6 +4,17 @@
 #include "stdafx.h"
 #include <GLFW\glfw3.h>
 
+void
+refresh(GLFWwindow *window)
+{
+	printf("Hello, world!\n");
+	
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+	return;
+}
+
 int
 main()
 {
@@ -18,11 +29,15 @@ main()
 	}
 
 	glfwMakeContextCurrent(window);
+	glClearColor(0.1f, 0.2f, 0.5f, 0.0f);
 
+	glfwSetWindowRefreshCallback(window, refresh);
+
+	// TODO: Find out how to enable VSync.
 	while (!glfwWindowShouldClose(window)) {
-		glClear(GL_COLOR_BUFFER_BIT);
-		glfwSwapBuffers(window);
-		glfwPollEvents();
+		//glClear(GL_COLOR_BUFFER_BIT);
+		//glfwSwapBuffers(window);
+		glfwWaitEvents();
 	}
 
 	glfwTerminate();
