9f6daaf1471a684456caf619bfc80ef4d7c43ec7
Sid Spry <R030t1@gmail.com>
2017-03-15 13:34:37 -0500

    Add initial window.

--- Console/Console.cpp
+++ Console/Console.cpp
@@ -7,9 +7,25 @@
 int
 main()
 {
+	GLFWwindow *window;
+
 	if (!glfwInit())
 		return -1;
 
+	if (!(window = glfwCreateWindow(640, 480, "Console", NULL, NULL))) {
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
     return 0;
 }
 
